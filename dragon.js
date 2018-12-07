16331473c60672a8cd5863d43d68af2612cdbc53
wyattlaberge64 <42870908+wyattlaberge64@users.noreply.github.com>
2018-12-07 11:24:59 -0500

    Fixed

--- dragon.js
+++ dragon.js
@@ -69,16 +69,17 @@ function checkAnswers(answer) {  // Matches Scenes  - replace with yours
 	 }
 }
 
-
+// L1S1
 function voiceInForest(){  // FIRST SCENE - WELCOMES PLAYER
 var name = prompt("what is your name?");
-story("You a young dragon are heading back home from you treasure hunting journey when suddenly you hear a voice from the forest saying \
-\n Come into the forest " + name + " your adventure is just beginning");
+story("You are a young dragon are heading back home from you treasure hunting journey. When suddenly you hear a voice from the forest saying \
+\n Come into the forest " + name + " your adventure is just beginning.");
 
   choices = ["Think More", "Go Forest", "Ignore It"];
   answer = setOptions(choices);
 } 
   
+ //L1S2
  function thinking() {  // SET A SCENE
   story("As you think about this you feel that this situation is very familiar to you.\
   \n That or your probably thinking about it to much.");
@@ -87,22 +88,25 @@ story("You a young dragon are heading back home from you treasure hunting journe
   answer = setOptions(choices);  
 }
   
+ //L1S3
  function forest() {
-  story("You go into the forest to see what sort of an adventure is in there\
-  \n and any secrets you find along the way.");
+  story("You go into the forest to see what sort of an adventure is in there.\
+  \n Also to hunt down any secrets in the forest.");
   
   choices = ["Go To Portal","Look Around(Forest)","Hang Out Here"];
   answer = setOptions(choices);
 } 
 
+ //L1S4
  function forestExamine() {
-  story("You look around the forest to see\
+  story("You look around the forest to see.\
   \n a portal on the ground, a item vender and a clearing the leads to an open field.");
   
   choices = ["Go To Portal","Go To Field","Go To Item Vender"];
   answer = setOptions(choices);
 }
-
+ 
+ //L1S5
  function portal() {
   story("You walk towards a glowing portal on the ground. \
   \n You are unsure about it and get an uneasy feeling about it.");
@@ -110,7 +114,8 @@ story("You a young dragon are heading back home from you treasure hunting journe
   choices = ["Step On To Portal","Go Back(forest)",""];
   answer = setOptions(choices);
 }
-
+ 
+ //L1S6
  function itemVender() {
   story("You head towards the item vender and you see that he has many items. Like \
   \n a healing potion, a apple pie, a bone, an antidote for poison and an axe \
@@ -119,23 +124,25 @@ story("You a young dragon are heading back home from you treasure hunting journe
   choices = ["Potion","Pie","The Bone","Antidote","Axe","Go Back(forest)"];
   answer = setOptions(choices);
 }
-
+ //L1S7
  function field() {
-  story("You head towards the clearing and go to the field to see what's ahead.");
+  story("You head towards the clearing. Heading to the field to see what's ahead.");
   
   choices = ["Look Around(Field)","Go Back(forest)",""];
   answer = setOptions(choices);
 }
-
+ 
+ //L1S8
  function fieldExamine() {
-  story("You look around the big open field to see \
-  \n a pipe that blends in well with grass in the field, a wolf that looks like its been injured \
+  story("You look around the big open field to see. \
+  \n A pipe that blends in well with grass in the field, a wolf that looks like its been injured \
   \n and a warlock that seems to be traveling.");
   
   choices = ["Go To Pipe","Go To Wolf","Go To Warlock","Go Back(forest)"];
   answer = setOptions(choices);
 }
-
+ 
+ //L1S9
  function pipe() {
   story("You walk up to the pipe and you see that it has an entrance at the top of it. \
   \n You think that this seems familiar.");
@@ -143,7 +150,8 @@ story("You a young dragon are heading back home from you treasure hunting journe
   choices = ["Go Into The Pipe","Go Back(Field)",""];
   answer = setOptions(choices);
 }
-
+ 
+ //L1S10
  function injuredWolf() {
   story("You head towards the wolf to take a closer look. \
    \n As you look over the wolf you see a lot of scratches on its body. \
@@ -153,7 +161,8 @@ story("You a young dragon are heading back home from you treasure hunting journe
   choices = ["Give Potion","Give Bone","Go Back(Field)"];
   answer = setOptions(choices);
 }
-
+ 
+ //L1S11
  function travelingWarlock() {
   story("As you walk towards the warlock you notice that he looks kind of orcish. \
   \n Then you suddenly get a scent that is very familiar to you. \
@@ -163,7 +172,8 @@ story("You a young dragon are heading back home from you treasure hunting journe
   choices = ["Talk To Him","Give Antidote","Give Pie","Go Back(Field)"];
   answer = setOptions(choices);
 }
-
+ 
+ //L1S12
  function sewer() {
   story("You jump into the to see where it will take you \
   \n and you are taken into a sewer. You think that there is a \
@@ -173,12 +183,13 @@ story("You a young dragon are heading back home from you treasure hunting journe
   answer = setOptions(choices);
 }
 
+ //L1S13
  function sewerExamine() {
   story("As you look around the sewer you do not see anything \
   \n that indicates that there are any secrets. \
   \n Maybe if you had companion that knows this sewer they could \
   \n find any secrets. But right now the smell of the sewer \
-  \n is making you want to throw up so you think it would be a \
+  \n is making you want to throw up. So you think it would be a \
   \n good idea to leave this place NOW!");
   
   choices = ["Go Back(Field)"];
@@ -186,9 +197,9 @@ story("You a young dragon are heading back home from you treasure hunting journe
 }
  
  function home() {
-  story("You decided to ignore the voice and go home\
-  \n as you are heading back home you think to yourself\
-  \n could there really have be a grand adventure in that forest.");
+  story("You decided to ignore the voice and go home.\
+  \n As you are heading back home you think to yourself.\
+  \n Could there really have be a grand adventure in that forest.");
 }
  
  
